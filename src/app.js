4b44eb2ed9328f54d30a89347ec3b26fce6487a3
andresgc04 <andresgc1997@outlook.com>
2023-11-26 23:43:16 -0400

    Creando Proyecto De Express
    CreandoEstructura Del Proyecto y Servidor Con Express

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const config = require("./config");
+
+const clientes = require("./modulos/clientes/rutas");
+
+const app = express();
+
+//Configuración:
+app.set("port", config.app.port);
+
+//rutas:
+app.use("/api/clientes", clientes);
+
+module.exports = app;
