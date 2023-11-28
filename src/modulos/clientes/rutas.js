ace0a15b4414fab32f112349bcac53c8e7fba00f
andresgc04 <andresgc1997@outlook.com>
2023-11-27 22:21:37 -0400

    Creando La Configuracion Para La Conexion A La Bases De Datos
    Creando la configuracion para poder conectarnos a la base de datos MySQL y optimizando la funcion de la peticion en la ruta para usarla con async y await

--- src/modulos/clientes/rutas.js
+++ src/modulos/clientes/rutas.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const respuesta = require("../../red/respuestas");
+const controlador = require("./controlador");
 
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  respuesta.success(req, res, "Todo OK Desde Clientes", 200);
+router.get("/", async function (req, res) {
+  const items = await controlador.todos();
+  respuesta.success(req, res, items, 200);
 });
 
 module.exports = router;
