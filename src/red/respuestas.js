d7c5eefc7aa3d5fc7de9e37663e0262398060a05
andresgc04 <andresgc1997@outlook.com>
2023-11-27 20:21:56 -0400

    Optimizando Codigo De Las Respuestas De Las Peticiones
    Optimizando el codigo para las respuestas de las peticiones, eliminando las constantes creadas para cambiarlo a utilizar valores por defectos en los parametros que solicita la funcion

--- src/red/respuestas.js
+++ src/red/respuestas.js
@@ -1,21 +1,15 @@
-exports.success = function (req, res, mensaje, status) {
-  const statusCode = status || 200;
-  const mensajeOk = mensaje || "";
-
-  res.status(statusCode).send({
+exports.success = function (req, res, mensaje = "", status = 200) {
+  res.status(status).send({
     error: false,
-    status: statusCode,
-    body: mensajeOk,
+    status: status,
+    body: mensaje,
   });
 };
 
-exports.error = function (req, res, mensaje, status) {
-  const statusCode = status || 500;
-  const mensajeError = mensaje || "Error Interno";
-
+exports.error = function (req, res, mensaje = "Error Interno", status = 500) {
   res.status(status).send({
     error: true,
-    status: statusCode,
-    body: mensajeError,
+    status: status,
+    body: mensaje,
   });
 };
